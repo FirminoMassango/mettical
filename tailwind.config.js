8ab8d887c7843f5a02ee2041900a3939fc058094
Firmino Massango <firminorafael0@gmail.com>
2023-08-13 13:52:46 +0200

    feat: new icons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,17 @@ export default {
     extend: {
       colors: {
         primary: {
-          100: '#009F5E',
-          50: '#00B26A',
+          100: '#00B26A',
+          200: '#009F5E',
         },
+        secondary: {
+          100: '#A9A9A9',
+          200: '#7E7E7E',
+          300: '#515151',
+        },
+        light: {
+          100: '#F5F5F5',
+        }
       }
     },
   },
