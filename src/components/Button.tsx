9b58b756ecbe77c50796b4442e7aa61cbcbf03f1
Firmino Massango <firminorafael0@gmail.com>
2023-08-20 19:30:38 +0200

    fix: improving the code readability

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,17 +11,17 @@ export default function Button(props: ButtonProps) {
 
   return (
     <>
-      {props.type === "default" ? (
+      { props.type === "default" ? (
         <button
           className={`grid h-10 w-10 place-content-center rounded-full hover:bg-primary-100/50 ${background_color}`}
         >
-          <Icon name={props.icon} />
+          <Icon name={ props.icon } />
         </button>
       ) : (
         <button className="grid h-16 w-16 place-content-center fixed bottom-9 right-4 rounded-full bg-primary-200 hover:bg-primary-100">
           <Icon name="add" />
         </button>
-      )}
+      ) }
     </>
   );
 }
