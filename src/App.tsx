5f5ca341c8631f84cd0905d76ee7083938355c32
Firmino Massango <firminorafael0@gmail.com>
2023-08-20 13:55:56 +0200

    fix: code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import Header from './components/Header'
-import Card from './components/Card'
+import Header from "./components/Header";
+import Card from "./components/Card";
+import FloatActionButton from "./components/FloatActionButton";
+import RemainingBudget from "./components/RemainingBudget";
 
 function App() {
   return (
     <>
-      <Header title="Despesa 1" budget="150,200.00" main/>
-      <div className='flex justify-between m-4'>
-        <span className='text-xl text-secondary-300 font-semibold'>Remanescente</span>
-        <span className='text-secondary-200'>200.00 MZN</span>
-      </div>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Header title="Despesa 1" budget="150,200.00" main />
+      <RemainingBudget />
+      <Card title="Despesa 1" value={10000} />
+      <Card title="Despesa 2" value={10000} />
+      <Card title="Despesa 3" value={10000} />
+      <Card title="Despesa 4" value={10000} />
+      <Card title="Despesa 5" value={10000} />
+      <Card title="Despesa 6" value={10000} />
+      <Card title="Despesa 7" value={10000} />
+      <FloatActionButton/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,36 +1,35 @@
-import addIcon from "../assets/icons/add.svg"
-import arrowLeftIcon from "../assets/icons/arrow_left.svg"
-import editIcon from "../assets/icons/edit.svg"
-import logoutIcon from "../assets/icons/logout.svg"
-import moneyIcon from "../assets/icons/money.svg"
-import moreIcon from "../assets/icons/more.svg"
-import sunIcon from "../assets/icons/sun.svg"
-import translateIcon from "../assets/icons/translate.svg"
-import trashIcon from "../assets/icons/trash.svg"
-import walletIcon from "../assets/icons/wallet.svg"
+import addIcon from "../assets/icons/add.svg";
+import arrowLeftIcon from "../assets/icons/arrow_left.svg";
+import editIcon from "../assets/icons/edit.svg";
+import logoutIcon from "../assets/icons/logout.svg";
+import moneyIcon from "../assets/icons/money.svg";
+import moreIcon from "../assets/icons/more.svg";
+import sunIcon from "../assets/icons/sun.svg";
+import translateIcon from "../assets/icons/translate.svg";
+import trashIcon from "../assets/icons/trash.svg";
+import walletIcon from "../assets/icons/wallet.svg";
+import warningIcon from "../assets/icons/warnig.svg";
 
 const icons = {
-    add: addIcon,
-    arrow_left: arrowLeftIcon,
-    edit: editIcon,
-    logout: logoutIcon,
-    money: moneyIcon,
-    more: moreIcon,
-    sun: sunIcon,
-    translate: translateIcon,
-    trash: trashIcon,
-    wallet: walletIcon
-}
+  add: addIcon,
+  arrow_left: arrowLeftIcon,
+  edit: editIcon,
+  logout: logoutIcon,
+  money: moneyIcon,
+  more: moreIcon,
+  sun: sunIcon,
+  translate: translateIcon,
+  trash: trashIcon,
+  wallet: walletIcon,
+  warning: warningIcon,
+};
 
 interface IconProps {
-    name: keyof typeof icons;
+  name: keyof typeof icons;
 }
 
-export default function Icon(props: IconProps){
+export default function Icon(props: IconProps) {
+  const iconSrc = icons[props.name];
 
-    const iconSrc = icons[props.name];
-
-    return <img src={ iconSrc } alt={ props.name } />;
-    
+  return <img src={iconSrc} alt={props.name} />;
 }
-
