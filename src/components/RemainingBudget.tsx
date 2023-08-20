caa833ea4dec55e9fc2e4302752ef9ec96666988
Firmino Massango <firminorafael0@gmail.com>
2023-08-20 13:55:19 +0200

    feat: this component will display the remaining budget

---
+++ src/components/RemainingBudget.tsx
@@ -0,0 +1,28 @@
+import Icon from "./Icon";
+
+interface RemainingBudgetProps {
+  isBudgetRemaing?: boolean;
+}
+
+export default function RemainingBudget(props: RemainingBudgetProps) {
+  return (
+    <>
+      <div className="flex justify-between mx-4 my-8">
+        <span className="text-xl text-secondary-300 font-semibold">
+          Remanescente
+        </span>
+        {props.isBudgetRemaing ? (
+          <div className="flex text-red-500">
+            <Icon name="warning" />
+            <div className="flex">
+              <span className="text-lg font-semibold ml-2">-200.00 </span>
+              <span className="text-lg font-semibold ml-2">MZN</span>
+            </div>
+          </div>
+        ) : (
+          <span className="text-secondary-200 text-lg font-semibold ml-2">200.00 MZN</span>
+        )}
+      </div>
+    </>
+  );
+}
