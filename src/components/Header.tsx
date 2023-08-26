f1c2ff1b90918c9a6bc7f1cb7f96deb336284d31
Firmino Massango <firminorafael0@gmail.com>
2023-08-26 17:23:11 +0200

    refactor: add semantic tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,16 +9,16 @@ interface HeaderProps {
 
 export default function Header(props: HeaderProps) {
   return (
-    <div className="h-52 bg-primary-200 rounded-b-3xl p-4 text-white ">
-      <div className="flex justify-between">
+    <header className="h-52 bg-primary-200 rounded-b-3xl p-4 text-white ">
+      <nav className="flex justify-between">
         {props.isMainScreen && <Button icon="arrow_left" type="default" fill />}
         <h1 className="text-2xl font-bold">{props.title}</h1>
         <Button icon="more" type="default" fill />
-      </div>
+      </nav>
       <div className="flex justify-between my-8">
         <div className="">
           <div className="">
-            <h1 className="uppercase text-base font-bold">Valor inicial</h1>
+            <h2 className="uppercase text-base font-bold">Valor inicial</h2>
           </div>
           <div className="bg-seconday-100 mt-4">
             <span className="text-2xl font-bold align-bottom align-text-bottom">
@@ -31,6 +31,6 @@ export default function Header(props: HeaderProps) {
           <img className="animate-custom-bounce" src={moneyIcon} alt="money" />
         </div>
       </div>
-    </div>
+    </header>
   );
 }
