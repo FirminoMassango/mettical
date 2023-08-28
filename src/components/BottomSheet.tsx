203b84726471e542b4da80149a6099673f64e10d
Firmino Massango <firminorafael0@gmail.com>
2023-08-28 20:39:45 +0200

    fix: improving style...

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 export default function BottomSheet() {
   return (
     <>
-      <div className="w-full h-64 bg-white border border-primary-100 rounded-t-2xl fixed bottom-0 p-4">
+      <div className="w-full h-72 bg-white border border-secondary-100 rounded-t-2xl fixed bottom-0 p-4">
         <Input type="text" name="tipo" />
         <Input type="text" name="tipo" />
         <Input type="text" name="tipo" />
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,15 +5,18 @@ interface InputProps {
 export default function Input(props: InputProps) {
   return (
     <>
-      <div className="flex flex-col mb-2">
-        <label htmlFor="" className="text-base font-semibold">
+      <div className="flex flex-col mb-4">
+        <label
+          htmlFor=""
+          className="text-base text-secondary-300 font-bold mb-2"
+        >
           {props.name}
         </label>
         <input
           type={props.type}
           name={props.name}
           id=""
-          className="h-10 border border-secondary-100 rounded"
+          className="h-12 p-4 border border-secondary-100 rounded-lg"
           placeholder="Indique o tipo de despesa. Ex: Faculdade"
         />
       </div>
