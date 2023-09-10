8eb2b12b691af8bac61070c472e0c2962fb8179a
Firmino Massango <firminorafael0@gmail.com>
2023-09-10 19:57:55 +0200

    refactor: pass the value dynamically

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,6 @@
 // import { FormProvider } from "../contexts/DisplayFormContext";
 import { useForm } from "../contexts/DisplayFormContext";
 import Icon from "./Icon";
-import BottomSheet from "./BottomSheet";
 
 interface ButtonProps {
   icon: string;
@@ -10,18 +9,6 @@ interface ButtonProps {
 }
 
 export default function Button(props: ButtonProps) {
-  // const isActive = useContext(DisplayFormContext);
-  // const { isActive, setIsActive } = useContext(DisplayFormContext);
-  // const isActive = true;
-  // const { isActive, setIsActive } = useState(false);
-  // const [isActive, setIsActive] = useState<boolean>(false);
-  // const [isActive, setIsActive] = useState(false);
-
-  // Function to update isActive
-  // const toggleIsActive = () => {
-  //   setIsActive(!isActive);
-  // };
-
   const { isActive, toggleForm } = useForm();
   const background_color = props.fill ? "bg-primary-100" : "";
 
@@ -43,8 +30,6 @@ export default function Button(props: ButtonProps) {
           <Icon name={props.icon} />
         </button>
       )}
-
-      {isActive && <BottomSheet />}
     </>
   );
 }
