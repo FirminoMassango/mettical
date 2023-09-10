2dd7b639d24081c36300fec041407b53a526bc24
Firmino Massango <firminorafael0@gmail.com>
2023-09-10 19:59:06 +0200

    refactor: DisplayFormContext implementation

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -5,21 +5,31 @@ import { useForm } from "../contexts/DisplayFormContext";
 
 export default function BottomSheet() {
   const currentScreen = useContext(CurrentScreenContext);
-  const { toggleForm } = useForm();
+  const { isActive, toggleForm } = useForm();
   return (
     <>
       <CurrentScreenContext.Provider value="bbbbb">
-        <div className="w-full h-72 bg-white border border-secondary-100 rounded-t-2xl fixed bottom-0 p-4">
-          <div className="flex justify-between">
-            {currentScreen}
-            <button className="text-red-800" onClick={toggleForm}>
-              close
-            </button>
+        {isActive && (
+          <div className="w-full h-2/5 bg-white border border-secondary-100 rounded-t-2xl fixed bottom-0 p-4">
+            <div className="flex justify-between mb-4">
+              {currentScreen}
+              <button className="text-red-800" onClick={toggleForm}>
+                close
+              </button>
+            </div>
+            <Input
+              type="text"
+              name="Tipo de despesa"
+              placeholder="Indique o tipo de despesa. Ex: Faculdade"
+            />
+            <Input
+              type="text"
+              name="Custo"
+              placeholder="Indique o custo da despesa. Ex: 5000"
+            />
+            <Input type="button" />
           </div>
-          <Input type="text" name="tipo" />
-          <Input type="text" name="tipo" />
-          <Input type="button" name="tipo" />
-        </div>
+        )}
       </CurrentScreenContext.Provider>
     </>
   );
--- src/screens/Bills.tsx
+++ src/screens/Bills.tsx
@@ -2,20 +2,14 @@ import Header from "../components/Header";
 import Card from "../components/Card";
 import RemainingBudget from "../components/RemainingBudget";
 import Button from "../components/Button";
-// import BottomSheet from "../components/BottomSheet";
 import { CurrentScreenContext } from "../contexts/CurrentScreenContext";
 import { CurrentExpenseDetailsContext } from "../contexts/CurrentExpenseDetailsContext";
 import { useContext } from "react";
-// import { useForm } from "../contexts/DisplayFormContext";
 import { FormProvider } from "../contexts/DisplayFormContext";
+import BottomSheet from "../components/BottomSheet";
 
 function Main() {
   const expenseDetails = useContext(CurrentExpenseDetailsContext);
-
-  // const { isActive } = useForm();
-
-  // console.log(isActive);
-  // const currentScreen = useContext(CurrentScreenContext);
 
   expenseDetails.title = "Lista de despesas";
   expenseDetails.budget = 9000;
@@ -39,7 +33,7 @@ function Main() {
             <Card title="Despesa 6" value={10000} screenName="expenses" />
             <Card title="Despesa 7" value={10000} screenName="expenses" />
             <Button icon="add" type="fab" />
-            {/* {isActive && <BottomSheet />} */}
+            <BottomSheet />
           </FormProvider>
         </CurrentExpenseDetailsContext.Provider>
       </CurrentScreenContext.Provider>
